53da94046cadd3f980158f9e2253bcfc08435364
mathiouchio <contact@matheusdesain.com>
2020-11-21 10:37:08 -0500

    true mui dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import reducer from './reducers'
 
 import Component from "./component";
@@ -12,6 +14,25 @@ import Component from "./component";
 const store = createStore(reducer, applyMiddleware(thunk))
 
 const darkTheme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      'Oxygen',
+      'Ubuntu',
+      'Cantarell',
+      '"Fira Sans"',
+      '"Droid Sans"',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
   palette: {
     type: "dark"
   }
@@ -20,6 +41,7 @@ const darkTheme = createMuiTheme({
 const App = () => {
   return (
     <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
       <Provider store={store}>
         <Component />
       </Provider>
